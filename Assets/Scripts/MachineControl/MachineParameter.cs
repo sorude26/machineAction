6f3866953e6d2bd8cf38e68e57024029ae2e95c8
sorude26 <sorude26@gmail.com>
2021-10-11 16:45:49 +0900

    機体のパラメータ管理用スクリプトを追加した

---
+++ Assets/Scripts/MachineControl/MachineParameter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineParameter : MonoBehaviour
+{
+    [Tooltip("歩行移動力")]
+    [SerializeField]
+    float m_walkSpeed = 10f;
+    public float WalkSpeed { get => m_walkSpeed; }
+    [Tooltip("歩行最高速度")]
+    [SerializeField]
+    float m_maxWalkSpeed = 5f;
+    public float MaxWalkSpeed { get => m_walkSpeed; }
+    [Tooltip("旋回速度")]
+    [SerializeField]
+    float m_turnSpeed = 5f;
+    public float TurnSpeed { get => m_turnSpeed; }
+}
