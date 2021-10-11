4e748bd1c566317ad7ca1deb09d872bc47a4c9f6
sorude26 <sorude26@gmail.com>
2021-10-11 17:06:46 +0900

    移動制御用スクリプトを追加した

---
+++ Assets/Scripts/MachineControl/MoveControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 移動を制御する
+/// </summary>
+public class MoveControl : MonoBehaviour
+{
+    public void MoveWalk(Rigidbody rb,Vector3 dir,float power,float maxSpeed)
+    {
+        if (rb.velocity.sqrMagnitude < maxSpeed)
+        {
+            rb.AddForce(dir * power);
+        }
+    }
+    public void MoveFloat(Rigidbody rb, Vector3 dir, float speed, float maxSpeed)
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        if (rb.velocity.sqrMagnitude < maxSpeed)
+        {
+            rb.velocity = dir * speed;
+        }
+    }
+}
