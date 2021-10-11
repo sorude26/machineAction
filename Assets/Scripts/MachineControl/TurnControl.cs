9032ea965fbe136ca404cb92f9d9027704f5e63b
sorude26 <sorude26@gmail.com>
2021-10-11 16:47:15 +0900

    方向制御用スクリプトを追加した

---
+++ Assets/Scripts/MachineControl/TurnControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 方向転換を制御する
+/// </summary>
+public class TurnControl : MonoBehaviour
+{
+    public bool TurnNow { get; private set; }
+    public void StartTurn(float turnSpeed, Vector3 dir)
+    {
+        if (TurnNow)
+        {
+            StopAllCoroutines();
+        }
+        StartCoroutine(Turn(turnSpeed, dir));
+    }
+    IEnumerator Turn(float turnSpeed, Vector3 targetDir)
+    {
+        TurnNow = true;
+        while (true)
+        {
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, turnSpeed * Time.deltaTime, 0f);
+            Quaternion turnRotation = Quaternion.LookRotation(newDir);
+            if (transform.rotation == turnRotation)
+            {
+                break;
+            }
+            transform.rotation = turnRotation;
+            yield return null;
+        }
+        TurnNow = false;
+    }
+}
