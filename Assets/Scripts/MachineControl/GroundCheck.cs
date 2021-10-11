ac30ac4991ff81d7a4e9d96de4dd05e1e42cc64c
sorude26 <sorude26@gmail.com>
2021-10-11 16:46:37 +0900

    設置判定用スクリプトを追加した

---
+++ Assets/Scripts/MachineControl/GroundCheck.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 設置判定を行う
+/// </summary>
+public class GroundCheck : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] m_leftCheckPos = default;
+    [SerializeField]
+    Transform[] m_rightCheckPos = default;
+    [SerializeField]
+    Vector3 m_checkDir = Vector3.down;
+    [SerializeField]
+    float m_checkRange = 0.2f;
+    public bool IsGrounded()
+    {
+        int leftCount = default;
+        int rightCount = default;
+        foreach (var pos in m_leftCheckPos)
+        {
+            Vector3 start = pos.position;
+            Vector3 end = start + m_checkDir * m_checkRange;
+            bool left = Physics.Linecast(start, end);
+            if (left)
+            {
+                leftCount++;
+                if (leftCount > 1)
+                {
+                    return true;
+                }
+            }
+        }
+        foreach (var pos in m_rightCheckPos)
+        {
+            Vector3 start = pos.position;
+            Vector3 end = start + m_checkDir * m_checkRange;
+            bool right = Physics.Linecast(start, end);
+            if (right)
+            {
+                rightCount++;
+                if (rightCount > 1)
+                {
+                    return true;
+                }
+            }
+        }
+        if (leftCount > 0 && rightCount > 0)
+        {
+            return true;
+        }
+        return false;
+    }
+}
