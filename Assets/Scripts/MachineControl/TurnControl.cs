637eb486ab03080a8474e488ca67f3bdb61265e4
sorude26 <sorude26@gmail.com>
2021-10-13 03:33:18 +0900

    方向転換をAddTorqueで行う機能を追加した

--- Assets/Scripts/MachineControl/TurnControl.cs
+++ Assets/Scripts/MachineControl/TurnControl.cs
@@ -8,28 +8,49 @@ using UnityEngine;
 public class TurnControl : MonoBehaviour
 {
     public bool TurnNow { get; private set; }
-    public void StartTurn(float turnSpeed, Vector3 dir)
+    float m_turn = 0;
+    public void StartTurn(Transform targetTransform, float turnSpeed, Vector3 dir)
     {
         if (TurnNow)
         {
             StopAllCoroutines();
         }
-        StartCoroutine(Turn(turnSpeed, dir));
+        StartCoroutine(Turn(targetTransform, turnSpeed, dir));
     }
-    IEnumerator Turn(float turnSpeed, Vector3 targetDir)
+    IEnumerator Turn(Transform tragetTransform,float turnSpeed, Vector3 targetDir)
     {
         TurnNow = true;
         while (true)
         {
-            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, turnSpeed * Time.deltaTime, 0f);
+            Vector3 newDir = Vector3.RotateTowards(tragetTransform.forward, targetDir, turnSpeed * Time.deltaTime, 0f);
             Quaternion turnRotation = Quaternion.LookRotation(newDir);
-            if (transform.rotation == turnRotation)
+            if (tragetTransform.rotation == turnRotation)
             {
                 break;
             }
-            transform.rotation = turnRotation;
+            tragetTransform.rotation = turnRotation;
             yield return null;
         }
         TurnNow = false;
+    }
+    public void Turn(Rigidbody rb, int dir, float speed,float maxSpeed)
+    {
+        if (rb.angularVelocity.sqrMagnitude < maxSpeed)
+        {
+            rb.AddTorque(0, dir * speed, 0);
+        }
+    }
+    void Turn(float q)
+    {
+        m_turn += q;
+        if (m_turn > 360)
+        {
+            m_turn -= 360;
+        }
+        if (m_turn < -360)
+        {
+            m_turn += 360;
+        }
+        Vector3 dir = Quaternion.Euler(0, m_turn, 0) * Vector3.forward;
     }
 }
