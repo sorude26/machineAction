55bbe5b42ce77e4a2995c0f2fc48cf3bbe24fa42
sorude26 <sorude26@gmail.com>
2021-11-06 19:32:14 +0900

    オブジェクトプールを作成する抽象クラスを作成した

---
+++ Assets/Scripts/ObjectPoolBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ObjectPoolBase<T,key> : MonoBehaviour where T : MonoBehaviour where key : Enum
+{
+    private static ObjectPoolBase<T,key> instance = default;
+
+    [SerializeField]
+    protected T[] m_objectPrefab = default;
+    [SerializeField]
+    protected int[] m_createCount = default;
+
+    Dictionary<key, T[]> m_objectDic = default;
+    private void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        if (m_objectPrefab.Length > m_createCount.Length)
+        {
+            return;
+        }
+        m_objectDic = new Dictionary<key, T[]>();
+        for (int i = 0; i < m_objectPrefab.Length; i++)
+        {
+            var bulletData = new T[m_createCount[i]];
+            for (int b = 0; b < m_createCount[i]; b++)
+            {
+                var bullet = Instantiate(m_objectPrefab[i], transform);
+                bullet.gameObject.SetActive(false);
+                bulletData[b] = bullet;
+            }
+            m_objectDic.Add((key)Enum.ToObject(typeof(key), i), bulletData);
+        }
+    }
+
+    public static T Get(key type, Vector3 pos)
+    {
+        foreach (var bullet in instance.m_objectDic[type])
+        {
+            if (bullet.gameObject.activeInHierarchy)
+            {
+                continue;
+            }
+            bullet.transform.position = pos;
+            bullet.gameObject.SetActive(true);
+            return bullet;
+        }
+        return null;
+    }
+}
