13ba78d09dace7cb3f4df72caba220085cbc4447
sorude26 <sorude26@gmail.com>
2021-11-06 19:33:50 +0900

    Effectのオブジェクトプールとパーティクル操作用のスクリプトを追加した

---
+++ Assets/Scripts/EffectControl.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectControl : MonoBehaviour
+{
+    [SerializeField]
+    ParticleSystem m_particleSystem = default;
+    public ParticleSystem Particle { get => m_particleSystem; }
+}
---
+++ Assets/Scripts/EffectPool.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EffectType
+{
+    ShotHit,
+    Bom,
+    Explosion,
+}
+public class EffectPool : ObjectPoolBase<EffectControl,EffectType>
+{
+    
+}
+
