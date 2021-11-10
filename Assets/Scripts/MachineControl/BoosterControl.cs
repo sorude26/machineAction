bcd0177fbb931cda1883a2ebc0417adaa7f1ce15
sorude26 <sorude26@gmail.com>
2021-11-10 20:39:03 +0900

    ブースト制御用スクリプトを追加した

---
+++ Assets/Scripts/MachineControl/BoosterControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoosterControl : MonoBehaviour
+{
+    [SerializeField]
+    ParticleSystem[] m_boost = default;
+    bool m_booster = false;
+    private void Start()
+    {
+        foreach (var item in m_boost)
+        {
+            item.Stop();
+        }
+    }
+    public void Boost()
+    {
+        foreach (var item in m_boost)
+        {
+            item.Play();
+        }
+    }
+    public void BoostEnd()
+    {
+        foreach (var item in m_boost)
+        {
+            item.Stop();
+        }
+    }
+}
