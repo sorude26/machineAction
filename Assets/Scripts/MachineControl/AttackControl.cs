ca5f6723daad46fb04da322c18f7ff745cd3ec43
sorude26 <sorude26@gmail.com>
2021-11-23 23:44:15 +0900

    攻撃操作を行うスクリプトを追加した

---
+++ Assets/Scripts/MachineControl/AttackControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackControl : MonoBehaviour
+{
+    string[] _lArmBlade = { "attackSwingLArm", "attackSwingLArm2", "attackSwingLArm3" };
+    string[] _rArmBlade = { "attackSwingRArm", "attackSwingRArm2", "attackSwingRArm3" };
+    string[] _dArmBlade = { "attackSwingDArm", "attackSwingDArm2", "attackSwingDArm3" };
+    string[] _dArmKnuckle = { "attackKnuckleRArm", "attackKnuckleLArm", "attackKnuckleRArm" };
+    string[] _lArmBladeRKnuckle = { "attackSwingLArm", "attackSwingLArm2", "attackKnuckleRArm" };
+    string[] _rArmBladeLKnuckle = { "attackSwingRArm", "attackSwingRArm2", "attackKnuckleLArm" };
+}
