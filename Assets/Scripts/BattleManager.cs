268c83fd0f060465b5131fbd2fe973c68f8362c9
sorude26 <sorude26@gmail.com>
2021-11-27 20:38:24 +0900

    戦闘管理のスクリプトを追加した

---
+++ Assets/Scripts/BattleManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    public static BattleManager Instance { get; private set; }
+    List<DamageControl> _allAttackTarget = default;
+    private void Awake()
+    {
+        Instance = this;
+        _allAttackTarget = new List<DamageControl>();
+    }
+
+    public void AddTarget(DamageControl target)
+    {
+        _allAttackTarget.Add(target);
+    }
+    public void ReMoveTarget(DamageControl target)
+    {
+        _allAttackTarget.Remove(target);
+    }
+    public DamageControl GetTarget(Transform attacker)
+    {
+        return _allAttackTarget.Where(target => Mathf.Abs(target.transform.position.y - attacker.position.y) < 20)
+            .Where(target => Vector3.Distance(target.Center.position, attacker.position) > 1f)
+            .Where(target => Vector3.Dot((target.Center.position - attacker.position).normalized, attacker.forward.normalized) > 0.4f)
+            .OrderBy(target => Vector3.Distance(target.Center.position, attacker.position)).FirstOrDefault();
+    }
+}
