2467330eaed15cb85c2c3c41b36f14b1a906fe9e
sorude26 <sorude26@gmail.com>
2021-11-28 23:03:24 +0900

    LockOnのUI表示機能を追加した

---
+++ Assets/Scripts/UI/TargetMark.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TargetMark : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _lockOnMark;
+    [SerializeField]
+    RectTransform _rect;
+    public DamageControl Target { get; private set; }
+    private void Update()
+    {
+        if (Target == null || Target.CurrentHP <= 0)
+        {
+            Target = null;
+            gameObject.SetActive(false);
+            return;
+        }
+        _rect.position = RectTransformUtility.WorldToScreenPoint(Camera.main, Target.Center.position);
+    }
+    public void SetTarget(DamageControl target)
+    {
+        Target = target;
+        _rect.position = RectTransformUtility.WorldToScreenPoint(Camera.main, Target.Center.position);
+        gameObject.SetActive(true);
+    }
+}
