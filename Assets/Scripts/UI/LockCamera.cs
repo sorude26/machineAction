f0dec7e57e5a52878ff343258d7ad3af06d22383
sorude26 <sorude26@gmail.com>
2021-12-03 16:00:08 +0900

    カメラの方を向き続けるスクリプトを追加した

---
+++ Assets/Scripts/UI/LockCamera.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockCamera : MonoBehaviour
+{
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform);
+    }
+}
