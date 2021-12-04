ee877a0e9cb7879f677d017994ab4e6cfc3a40c5
sorude26 <sorude26@gmail.com>
2021-12-04 19:38:00 +0900

    Animationに連携して攻撃を行う武器のスクリプトを追加した

---
+++ Assets/Scripts/AnimationShotWeapon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationShotWeapon : ShotWeapon
+{
+    protected override IEnumerator BulletShot()
+    {
+        _anime.SetFloat("BarrelSpeed", _shotInterval);
+        _anime.Play("startShot");
+        while (_shotCount > 0)
+        {
+            yield return null;
+        }
+        _anime.Play("endShot");
+        ShotNow = false;
+    }
+    private void AnimationShot()
+    {
+        _shotCount--;
+        Shot();
+    }
+    public override float AttackSpeed()
+    {
+        return _power;
+    }
+}
