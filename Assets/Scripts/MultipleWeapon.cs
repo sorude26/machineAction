f6463d1b88e4b16b730b967ee832d45ca9154fa6
sorude26 <sorude26@gmail.com>
2021-12-04 19:37:26 +0900

    複数の武器を操作するスクリプトを追加した

---
+++ Assets/Scripts/MultipleWeapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultipleWeapon : WeaponMaster
+{
+    [SerializeField]
+    ShotWeapon[] _weapons = default;
+    public override void AttackAction()
+    {
+        foreach (var weapon in _weapons)
+        {
+            weapon.AttackAction();
+        }
+    }
+    public override float AttackSpeed()
+    {
+        return _weapons[0].AttackSpeed();
+    }
+}
