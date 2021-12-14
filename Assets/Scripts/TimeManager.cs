3f0a9da7bc057dee97f56c5eec6a93d1d57738f2
sorude26 <sorude26@gmail.com>
2021-12-14 20:35:09 +0900

    時間管理用スクリプトを追加した

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameScene
+{
+    public class TimeManager : MonoBehaviour
+    {
+        static TimeManager instance = default;
+        [SerializeField]
+        GameObject _timeEffect = default;
+        bool _slow = false;
+        float timer = 0;
+        public static TimeManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    var obj = new GameObject("TimeManager");
+                    var manager = obj.AddComponent<TimeManager>();
+                    instance = manager;
+                }
+                return instance;
+            }
+        }
+        private void Awake()
+        {
+            instance = this;
+        }
+        public void HitStop()
+        {
+            timer = 0.3f;
+            if (!_slow)
+            {
+                _slow = true;
+                if (_timeEffect != null)
+                {
+                    _timeEffect.SetActive(true);
+                }
+                StartCoroutine(SlowTime());
+            }
+        }
+        IEnumerator SlowTime()
+        {
+            Time.timeScale = 0.3f;
+            while (timer > 0)
+            {
+                timer -= Time.unscaledDeltaTime;
+                yield return null;
+            }
+            Time.timeScale = 1f;
+            _slow = false; 
+            if (_timeEffect != null)
+            {
+                _timeEffect.SetActive(false);
+            }
+        }
+    }
+}
