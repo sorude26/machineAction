c74428de6cc25590fec246695556f00d9bab156d
sorude26 <sorude26@gmail.com>
2021-12-28 20:17:15 +0900

    回転する機能のスクリプトを追加した

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    [SerializeField]
+    float _rotateSpeed = -0.1f;
+    private void LateUpdate()
+    {
+        transform.Rotate(Vector3.up, _rotateSpeed);
+    }
+}
