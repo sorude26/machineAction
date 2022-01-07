a83f4dce1b66179f07effff4942352d82772636c
sorude26 <sorude26@gmail.com>
2022-01-07 14:55:02 +0900

    カスタマイズをゲームパッド操作に対応させた

--- Assets/Scripts/Customize/CustomizeControl.cs
+++ Assets/Scripts/Customize/CustomizeControl.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class CustomizeControl : MonoBehaviour
 {
     [SerializeField]
-    UnitBuildData _unitBuildData = default;
+    UnitBuildData _currentBuildData = default;
     [SerializeField]
     MachineBuildControl _buildControl = default;
     [SerializeField]
@@ -15,8 +15,18 @@ public class CustomizeControl : MonoBehaviour
     Slider[] _sliders = default;
     [SerializeField]
     Slider[] _colorSliders = new Slider[3];
+    [SerializeField]
+    Button _button = default;
+    [SerializeField]
+    GameObject _mark = default;
+    UnitBuildData _buildData = default;
+    int _targetMaxNumber = default;
+    int _targetNumber = 0;
+    float _changetime = 1f;
     Color _color = default;
     bool _setColor = false;
+    bool _inputStop = false;
+    bool _buttonOn = false;
     PartsManager _partsManager = default;
     private void Awake()
     {
@@ -24,22 +34,27 @@ public class CustomizeControl : MonoBehaviour
     }
     private void Start()
     {
-        _unitBuildData = GameManager.Instance.CurrentBuildData;
+        _buildData = GameManager.Instance.CurrentBuildData;
+        _currentBuildData = _buildData;
         _color = GameManager.Instance.PlayerColor;
-        _sliders[0].value = _unitBuildData.HeadID;
-        _sliders[1].value = _unitBuildData.BodyID;
-        _sliders[2].value = _unitBuildData.LArmID;
-        _sliders[3].value = _unitBuildData.RArmID;
-        _sliders[4].value = _unitBuildData.LegID;
-        _sliders[5].value = _unitBuildData.WeaponLArmID;
-        _sliders[6].value = _unitBuildData.WeaponRArmID;
-        _sliders[7].value = _unitBuildData.ShoulderWeaponID;
-        _sliders[8].value = _unitBuildData.BodyWeaponID;
+        _sliders[0].value = _currentBuildData.HeadID;
+        _sliders[1].value = _currentBuildData.BodyID;
+        _sliders[2].value = _currentBuildData.LArmID;
+        _sliders[3].value = _currentBuildData.RArmID;
+        _sliders[4].value = _currentBuildData.LegID;
+        _sliders[5].value = _currentBuildData.WeaponLArmID;
+        _sliders[6].value = _currentBuildData.WeaponRArmID;
+        _sliders[7].value = _currentBuildData.ShoulderWeaponID;
+        _sliders[8].value = _currentBuildData.BodyWeaponID;
         _colorSliders[0].value = _color.r;
         _colorSliders[1].value = _color.g;
         _colorSliders[2].value = _color.b;
+        _targetMaxNumber = _sliders.Length + _colorSliders.Length + 1;
         _setColor = true;
+        GameScene.InputManager.Instance.OnInputAxisRaw += MoveCursor;
+        GameScene.InputManager.Instance.OnFirstInputJump += ChangeOn;
         Build();
+        TargetControl(_targetNumber, 0);
         FadeController.StartFadeIn();
     }
     public void Build()
@@ -49,7 +64,7 @@ public class CustomizeControl : MonoBehaviour
             transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
         _partsManager.ResetAllParts();
-        _buildControl.SetData(_unitBuildData);
+        _buildControl.SetData(_currentBuildData);
         _buildControl.StartSet(_partsManager);
         _modelTransform[0].localRotation = Quaternion.Euler(0, 0, -6);
         _modelTransform[1].localRotation = Quaternion.Euler(0, 0, 6);
@@ -61,54 +76,203 @@ public class CustomizeControl : MonoBehaviour
     {
         _partsManager.ChangeColor(_color);
     }
+    void MoveCursor(float v, float h)
+    {
+        if (_inputStop)
+        {
+            return;
+        }
+        if (h > 0.8f || h < -0.8f)
+        {
+            if (h > 0)
+            {
+                UIControl(1, 0);
+            }
+            else
+            {
+                UIControl(-1, 0);
+            }
+        }
+        else if (v > 0.8f || v < -0.8f)
+        {
+            if (v > 0)
+            {
+                UIControl(0, 1);
+            }
+            else
+            {
+                UIControl(0, -1);
+            }
+        }
+    }
+    void UIControl(int target, int value)
+    {
+        _changetime = 0.1f;
+        if (target < 0)
+        {
+            _targetNumber++;
+            if (_targetNumber >= _targetMaxNumber)
+            {
+                _targetNumber = 0;
+            }
+            value = 0;
+            InputStop();
+        }
+        else if (target > 0)
+        {
+            _targetNumber--; 
+            if (_targetNumber < 0)
+            {
+                _targetNumber = _targetMaxNumber;
+            }
+            value = 0;
+            InputStop();
+        }
+        TargetControl(_targetNumber, value);
+    }
+    void TargetControl(int target,int value)
+    {
+        if (target < _sliders.Length)
+        {
+            _buttonOn = false;
+            _mark.transform.position = _sliders[target].transform.position;
+            PartsChange(target, value);
+        }
+        else if (target < _targetMaxNumber - 1)
+        {
+            _buttonOn = false;
+            _mark.transform.position = _colorSliders[target - _sliders.Length].transform.position;
+            ColorChange(target - _sliders.Length, value);
+        }
+        else
+        {
+            _buttonOn = true;
+            _mark.transform.position = _button.transform.position;
+        }
+    }
+    void PartsChange(int target,int count)
+    {
+        if (_inputStop)
+        {
+            return;
+        }
+        _changetime = 0.1f;
+        if (count > 0)
+        {
+            if (_sliders[target].maxValue > _sliders[target].value)
+            {
+                _sliders[target].value++;
+                InputStop();
+            }
+        }
+        else if (count < 0)
+        {
+            if (_sliders[target].value > 0)
+            {
+                _sliders[target].value--;
+                InputStop();
+            }
+        }
+    }
+    void ColorChange(int target,int count)
+    {
+        if (_inputStop)
+        {
+            return;
+        }
+        _changetime = 0.02f;
+        if (count > 0)
+        {
+            if (_colorSliders[target].maxValue > _colorSliders[target].value)
+            {
+                _colorSliders[target].value += 0.01f;
+            }
+            InputStop();
+        }
+        else if (count < 0)
+        {
+            if (_colorSliders[target].value > 0)
+            {
+                _colorSliders[target].value -= 0.01f;
+            }
+            InputStop();
+        }
+    }
+    void ChangeOn()
+    {
+        if (_inputStop || !_buttonOn)
+        {
+            return;
+        }
+        GameScene.InputManager.Instance.InputActionsOut();
+        _button.onClick?.Invoke();
+    }
+    void InputStop()
+    {
+        if (_inputStop)
+        {
+            return;
+        }
+        _inputStop = true;
+        StartCoroutine(InputWait());
+    }
+    IEnumerator InputWait()
+    {
+        while (_changetime > 0)
+        {
+            _changetime -= Time.deltaTime;
+            yield return null;
+        }
+        _inputStop = false;
+    }
     public void SetDataBattlleStart()
     {
-        GameManager.Instance.SetData(_unitBuildData, _color);
+        GameManager.Instance.SetData(_currentBuildData, _color);
         SceneChange.RoadGame();
     }
     public void ChangeHead(int number)
     {
-        _unitBuildData.HeadID = (int)_sliders[number].value;
+        _currentBuildData.HeadID = (int)_sliders[number].value;
         Build();
     }
     public void ChangeBody(int number)
     {
-        _unitBuildData.BodyID = (int)_sliders[number].value;
+        _currentBuildData.BodyID = (int)_sliders[number].value;
         Build();
     }
     public void ChangeLArm(int number)
     {
-        _unitBuildData.LArmID = (int)_sliders[number].value;
+        _currentBuildData.LArmID = (int)_sliders[number].value;
         Build();
     }
     public void ChangeRArm(int number)
     {
-        _unitBuildData.RArmID = (int)_sliders[number].value;
+        _currentBuildData.RArmID = (int)_sliders[number].value;
         Build();
     }
     public void ChangeLeg(int number)
     {
-        _unitBuildData.LegID = (int)_sliders[number].value;
+        _currentBuildData.LegID = (int)_sliders[number].value;
         Build();
     }
     public void ChangeLWeapon(int number)
     {
-        _unitBuildData.WeaponLArmID = (int)_sliders[number].value;
+        _currentBuildData.WeaponLArmID = (int)_sliders[number].value;
         Build();
     }
     public void ChangeRWeapon(int number)
     {
-        _unitBuildData.WeaponRArmID = (int)_sliders[number].value;
+        _currentBuildData.WeaponRArmID = (int)_sliders[number].value;
         Build();
     }
     public void ChangeShoulder(int number)
     {
-        _unitBuildData.ShoulderWeaponID = (int)_sliders[number].value;
+        _currentBuildData.ShoulderWeaponID = (int)_sliders[number].value;
         Build();
     }
     public void ChangeBWeapon(int number)
     {
-        _unitBuildData.BodyWeaponID = (int)_sliders[number].value;
+        _currentBuildData.BodyWeaponID = (int)_sliders[number].value;
         Build();
     }
     public void ChangeColor()
