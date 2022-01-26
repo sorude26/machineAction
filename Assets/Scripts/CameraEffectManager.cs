1d6802d4d6045e42d59c9d57f62dc44d6b8ac24f
sorude26 <sorude26@gmail.com>
2022-01-26 19:03:21 +0900

    画面振動等を操作するスクリプトを追加した

---
+++ Assets/Scripts/CameraEffectManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraEffectManager : MonoBehaviour
+{
+    static CameraEffectManager instance = default;
+    [SerializeField]
+    float _shakeRange = 300f;
+    [SerializeField]
+    float _shakePower = 5f;
+    [SerializeField]
+    float _lightShakeRange = 50f;
+    [SerializeField]
+    float _lightShakePower = 1f;
+    [SerializeField]
+    ShakeControl _cameraShakeControl = default;
+    private void Awake()
+    {
+        instance = this;
+    }
+    public static void Shake(Vector3 pos)
+    {
+        var range = (BattleManager.Instance.PlayerPos.position - pos).magnitude;
+        if (range < instance._shakeRange)
+        {
+            instance._cameraShakeControl.StartShake(instance._shakePower / range, 1.8f);
+        }
+    }
+    public static void LightShake(Vector3 pos)
+    {
+        var range = (BattleManager.Instance.PlayerPos.position - pos).magnitude;
+        if (range < instance._lightShakeRange)
+        {
+            instance._cameraShakeControl.StartShake(instance._lightShakePower / range, 0.8f);
+        }
+    }
+}
