975252ed40e8e19a04ef95f2936d0340eb108c53
sorude26 <sorude26@gmail.com>
2022-02-09 16:21:06 +0900

    カメラの中心から離れた時にマークを非表示にする処理を追加した

--- Assets/Scripts/UI/TargetMark.cs
+++ Assets/Scripts/UI/TargetMark.cs
@@ -6,9 +6,13 @@ using UnityEngine.UI;
 public class TargetMark : MonoBehaviour
 {
     [SerializeField]
+    RectTransform _center;
+    [SerializeField]
     GameObject _lockOnMark;
     [SerializeField]
     RectTransform _rect;
+    [SerializeField]
+    float _range = 0.4f;
     public DamageControl Target { get; private set; }
     private void Update()
     {
@@ -19,6 +23,11 @@ public class TargetMark : MonoBehaviour
             return;
         }
         _rect.position = RectTransformUtility.WorldToScreenPoint(Camera.main, Target.Center.position);
+        if((_center.position - _rect.position).magnitude > _range)
+        {
+            Target = null;
+            gameObject.SetActive(false);
+        }
     }
     public void SetTarget(DamageControl target)
     {
