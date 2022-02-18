cbcbf91b5cee203465ea24bec7d5ff5257ea8581
sorude26 <sorude26@gmail.com>
2022-02-18 23:10:38 +0900

    定数部分をまとめたスクリプトを追加した

---
+++ Assets/Scripts/InputStatus.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputStatus
+{
+    public const float InputLimit = 0.5f;
+    public const float MoveLimit = 0.1f;
+}
---
+++ Assets/Scripts/MachineControl/MachineStatus.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineStatus
+{
+    public const float BrakePower = 0.3f;
+    public const float Turn = 1f;
+    public const float TurnPower = 0.2f;
+    public const float TurnMovePower = 0.05f;
+    public const float FlyTurnDelay = 0.05f;
+    public const float FlyBodyReset = 0.8f;
+    public const float LandingBodyReset = 0.1f;
+    public const float BodyLock = 0.6f;
+    public const float JetLimit = 1f;
+    public const float AttackRange = 0.999f;
+    public const float StrongTurn = 8f;
+    public const float ExplosionWait = 3f;
+}
