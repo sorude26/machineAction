bc2bbbce9b9fd61c593b0315b320fadccb282e1e
sorude26 <sorude26@gmail.com>
2022-02-23 23:42:50 +0900

    敵を出現させる関数を追加した

--- Assets/Scripts/StageSelect/EnemyData.cs
+++ Assets/Scripts/StageSelect/EnemyData.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class EnemyData : MonoBehaviour
 {
-    
+    [SerializeField]
+    EnemyContorller[] _stageEnemys = default;
+    [SerializeField]
+    Transform[] _enemySpawnPoints = default;
+    public void SpawnEnemy()
+    {
+        foreach (var enemy in _stageEnemys)
+        {
+            enemy.StartSet();
+        }
+    }
 }
