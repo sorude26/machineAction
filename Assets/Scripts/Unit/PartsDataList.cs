8e6119409fa351015c95dd11944a51f79be6e8b0
sorude26 <sorude26@gmail.com>
2022-03-03 19:09:22 +0900

    パラメータデータスクリプトを追加した

---
+++ Assets/Scripts/Unit/PartsDataList.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartsDataList : ScriptableObject
+{
+    [SerializeField]
+    HeadData _headData = default;
+    [SerializeField]
+    BodyData _bodyData = default;
+    [SerializeField]
+    ArmData _armData = default;
+    [SerializeField]
+    LegData _legData = default;
+    [SerializeField]
+    BoosterData _boosterData = default;
+
+}
